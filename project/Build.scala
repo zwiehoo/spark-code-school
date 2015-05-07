615370d2823a86f940095537b6894b7f20236cc7
Tomasz Zwierzchowski <tomasz.zwierzchowski@allegro.pl>
2015-05-07 12:17:08 +0200

    Code school v1

---
+++ project/Build.scala
@@ -0,0 +1,73 @@
+import sbt._
+import sbt.Keys._
+
+object BuildSettings {
+
+  val Name = "spark-kafka-ml"
+  val Version = "2.1"
+  val ScalaVersion = "2.10.5"
+
+  lazy val buildSettings = Defaults.coreDefaultSettings ++ Seq (
+    name          := Name,
+    version       := Version,
+    scalaVersion  := ScalaVersion,
+    organization  := "pl.allegro",
+    description   := "Spark Streaming Kafka Machine Learning",
+    scalacOptions := Seq("-deprecation", "-unchecked", "-encoding", "utf8", "-Xlint")
+  )
+}
+
+
+object Resolvers {
+  val typesafe = "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
+  val sonatype = "Sonatype Release" at "https://oss.sonatype.org/content/repositories/releases"
+  val mvnrepository = "MVN Repo" at "http://mvnrepository.com/artifact"
+
+  val allResolvers = Seq(typesafe, sonatype, mvnrepository)
+
+}
+
+object Dependency {
+  object Version {
+    val Spark        = "1.2.1"
+    val ScalaTest    = "2.1.4"
+    val ScalaCheck   = "1.11.3"
+  }
+
+  val sparkCore      = "org.apache.spark"  %% "spark-core"      % Version.Spark  withSources()
+  val sparkStreaming = "org.apache.spark"  %% "spark-streaming" % Version.Spark  withSources()
+  val sparkMLlib     = "org.apache.spark"  %% "spark-mllib"     % Version.Spark  withSources()
+
+  val kafkaStream    = "org.apache.spark"  %% "spark-streaming-kafka" % Version.Spark withSources()
+  val scalaTest      = "org.scalatest"     %% "scalatest"       % Version.ScalaTest  % "test"
+  val scalaCheck     = "org.scalacheck"    %% "scalacheck"      % Version.ScalaCheck % "test"
+}
+
+object Dependencies {
+  import Dependency._
+
+  val sparkWorkshop =
+    Seq(sparkCore, sparkStreaming, //sparkMLlib,
+      scalaTest, scalaCheck, kafkaStream)
+}
+
+object KafkaStreamingBuild extends Build {
+  import Resolvers._
+  import BuildSettings._
+
+  lazy val kafkaStreamingProject = Project(
+    id = "Kafka-Streaming",
+    base = file("."),
+    settings = buildSettings ++ Seq(
+      maxErrors          := 5,
+      triggeredMessage   := Watched.clearWhenTriggered,
+      resolvers := allResolvers,
+      libraryDependencies ++= Dependencies.sparkWorkshop,
+      unmanagedResourceDirectories in Compile += baseDirectory.value / "conf",
+      mainClass := Some("run"),
+      fork := true,
+      parallelExecution in Test := false))
+}
+
+
+
---
+++ src/main/scala/pl/allegro/codeschool/spark/KafkaDuplicates.scala
@@ -0,0 +1,46 @@
+package pl.allegro.codeschool.spark
+
+import org.apache.spark.SparkContext
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.StreamingContext._
+import OfferPublicationStarted.{Price, Name, SellerId, CountryCode}
+
+object Settings {
+  val duration = Seconds(10)
+  val windowSize = Seconds(60)
+}
+
+object KafkaDuplicates extends App {
+  val sc = new SparkContext("local[8]", "wiki-spark-app")
+  sc.setCheckpointDir("output/checkpointy")
+
+  val ssc: StreamingContext = new StreamingContext(sc, Settings.duration)
+
+  val stream = OfferPublicationStarted.stream(ssc)
+
+  val message = stream.map(OfferPublicationStarted.message)
+
+  val result = message.map(OfferPublicationStarted.parse)
+    .filter {
+    case (_, _, CountryCode(countryCode), _, _) => countryCode == "PL"
+  }
+    .map {
+    case (SellerId(sellerId), Name(name), _, _, Price(price)) => (sellerId, name, price)
+  }
+    .map(x => (x, 1))
+    .reduceByKeyAndWindow(
+      reduceFunc = _ + _,
+      invReduceFunc = _ - _,
+      windowDuration = Settings.windowSize
+    )
+    .filter {
+      case (triple, count) => count > 1
+    }
+    .transform(_.sortBy(_._2, ascending = false))
+    .cache
+
+  result.print()
+
+  ssc.start()
+  ssc.awaitTermination()
+}
