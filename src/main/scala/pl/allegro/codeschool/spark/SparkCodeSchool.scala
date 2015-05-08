30e2756cd68124735808e3079d4b4099fae0845c
Tomasz Zwierzchowski <tomasz.zwierzchowski@allegro.pl>
2015-05-08 08:30:41 +0200

    gitignore

--- src/main/scala/pl/allegro/codeschool/spark/SparkCodeSchool.scala
+++ src/main/scala/pl/allegro/codeschool/spark/SparkCodeSchool.scala
@@ -1,5 +1,19 @@
 package pl.allegro.codeschool.spark
 
-class SparkCodeSchool {
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
 
+object SparkCodeSchool extends App {
+  val sc = new SparkContext("local[8]", "my-spark-app")
+
+  val input = sc.textFile("input/all-shakespeare.txt")
+
+  val words = input.map(_.toLowerCase.trim).flatMap(_.split("""[ ]+""")).cache()
+
+  words
+    .filter(!_.isEmpty)
+    .keyBy(word => 1).map (_.swap)
+    .reduceByKey((a,b) => a + b)
+    .sortBy(_._2, ascending = false)
+    .take(100) foreach println
 }
