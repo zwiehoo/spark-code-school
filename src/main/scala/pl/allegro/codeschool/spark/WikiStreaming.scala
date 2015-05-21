9fe60b3aa1dab84d636a73aa96aeac29a8318846
Tomasz Zwierzchowski <tomasz.zwierzchowski@allegro.pl>
2015-05-21 14:35:48 +0200

    Import required for reduceByKey

--- src/main/scala/pl/allegro/codeschool/spark/WikiStreaming.scala
+++ src/main/scala/pl/allegro/codeschool/spark/WikiStreaming.scala
@@ -3,6 +3,7 @@ package pl.allegro.codeschool.spark
 import com.lambdaworks.jacks.JacksMapper
 import org.apache.spark.SparkContext
 import org.apache.spark.streaming._
+//import org.apache.spark.streaming.StreamingContext._ // don't auto-optimize me!
 
 object WikiStreaming extends App {
   val sc = new SparkContext("local[8]", "my-spark-streaming-app")
@@ -10,7 +11,7 @@ object WikiStreaming extends App {
 
   val ssc: StreamingContext = new StreamingContext(sc, Seconds(10))
 
-  val stream = ssc.socketTextStream("localhost", 8124).map(JacksMapper.readValue[Map[String, Any]](_))
+  val stream = ssc.socketTextStream("localhost", 8124).map(JacksMapper.readValue[Map[String, Any]](_)).cache
 
   stream.print()
 
