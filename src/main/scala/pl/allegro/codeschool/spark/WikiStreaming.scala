82ba07c3107d777ad8fce51a68007fe562dfb160
Tomasz Zwierzchowski <tomasz.zwierzchowski@allegro.pl>
2015-05-21 14:36:50 +0200

    Import

--- src/main/scala/pl/allegro/codeschool/spark/WikiStreaming.scala
+++ src/main/scala/pl/allegro/codeschool/spark/WikiStreaming.scala
@@ -3,7 +3,7 @@ package pl.allegro.codeschool.spark
 import com.lambdaworks.jacks.JacksMapper
 import org.apache.spark.SparkContext
 import org.apache.spark.streaming._
-//import org.apache.spark.streaming.StreamingContext._ // don't auto-optimize me!
+import org.apache.spark.streaming.StreamingContext._ // don't auto-optimize me!
 
 object WikiStreaming extends App {
   val sc = new SparkContext("local[8]", "my-spark-streaming-app")
